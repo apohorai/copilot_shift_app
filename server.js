fa1895e8b210cc145a4908cda93aa4c49da816d4
apohorai <attila.pohorai@gmail.com>
2024-10-01 09:13:40 +0000

    user admin page added

--- server.js
+++ server.js
@@ -62,3 +62,10 @@ app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
 
+const path = require('path');
+
+// Serve the HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'users.html'));
+});
+
